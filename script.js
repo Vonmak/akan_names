264d2f1162abc1e4fa42ff322d490c36593b9fe1
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-03-07 00:30:52 +0300

    added the DOM that called elements in html by id

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ function myDate() {
   let one = document.getElementById("male");
   let two = document.getElementById("female");
 
-  // these are arrays for the are the akan names given by gender
-  let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  // // these are arrays for the are the akan names given by gender
+  // let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  // let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-  // used this method to calculate the local time selected by the user
-  let d = new Date(thedate);
+  // // used this method to calculate the local time selected by the user
+  // let d = new Date(thedate);
 
-  // this picks the day of the week from the day date selected
-  let day = d.getDay();
+  // // this picks the day of the week from the day date selected
+  // let day = d.getDay();
 
-  // checks wether the gender boxes have been selected
-  if (one.checked === true) {
-    display.innerHTML = "Your Akan name is:   " + male[day];
-  } else if (two.checked === true) {
-    display.innerHTML = "Your Akan name is:   " + female[day];
-  } else {
-    alert("Please select given data on the form provided!!");
-  }
+  // // checks wether the gender boxes have been selected
+  // if (one.checked === true) {
+  //   display.innerHTML = "Your Akan name is:   " + male[day];
+  // } else if (two.checked === true) {
+  //   display.innerHTML = "Your Akan name is:   " + female[day];
+  // } else {
+  //   alert("Please select given data on the form provided!!");
+  // }
 }
